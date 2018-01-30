8a23fad8d79be1cafc92928f8e8adf1cdd515c49
Ivan Chaban <ivan.chaban@devico.io>
2018-01-30 17:08:58 +0200

    clear

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,17 +5,17 @@ const path = require('path');
 const db = require('../db/db');
 
 function routes(app) {
-  var router = new Router();
+  const router = new Router();
 
   router
-    .get("/", async (ctx, next) => {
-      ctx.set('Content-Type', 'text/html');
-      ctx.body = fs.readFileSync(path.resolve(__dirname, '../../src/index.html'));
-    })
     .get('/listTodos', db.listTodos)
     .post('/addTodo', db.addTodo)
     .put('/updateTodo', db.updateTodo)
     .del('/:id', db.del)
+    .get("*", async (ctx, next) => {
+      ctx.set('Content-Type', 'text/html');
+      ctx.body = fs.readFileSync(path.resolve(__dirname, '../../src/index.html'));
+    })
 
   app.use(router.routes());
 }
--- server/server.js
+++ server/server.js
@@ -11,13 +11,33 @@ const routes = require('./routes/');
 const app = new Koa();
 db(app);
 
-const PORT = process.env.PORT || 1616;
+const PORT = process.env.PORT || 3000;
 
 app.use(middleware())
 app.use(koaBody())
 
 routes(app);
 
+app.use(async function pageNotFound(ctx) {
+  // we need to explicitly set 404 here
+  // so that koa doesn't assign 200 on body=
+  ctx.status = 404;
+
+  switch (ctx.accepts('html', 'json')) {
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = '<p>Page Not Found</p>';
+      break;
+    case 'json':
+      ctx.body = {
+        message: 'Page Not Found'
+      };
+      break;
+    default:
+      ctx.type = 'text';
+      ctx.body = 'Page Not Found';
+  }
+});
 
 if (require.main === module) {
   app.listen(PORT, function() {
